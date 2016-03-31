d33d6e8ced1d2b72996c5697de4ed0a6682ceefe
Seth Baughman <sbaughman1@me.com>
2016-03-31 14:03:19 -0400

    add simple_form gem, index and new pages for links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,26 @@
 class LinksController < ApplicationController
+
+  def index
+    @links = Link.order(votes: :desc)
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url, :summary)
+  end
+
 end
