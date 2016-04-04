5b25d5a1a0c78bcbc6a9d32230243ad455bda2c6
Seth Baughman <sbaughman1@me.com>
2016-04-04 14:55:19 -0400

    add methods for current_user, change layout based on whether user logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if @user = User.find_by_username(params[:session][:username]).try(:authenticate, params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:success] = "#{@user.username} successfully logged in"
+      redirect_to :root
+    else
+      flash[:danger] = "Not a valid username and/or password"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to :root
   end
 
 end
