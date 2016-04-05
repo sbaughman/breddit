e5bb749165e3b7e161e24dd316076ae9e2e40a55
Seth Baughman <sbaughman1@me.com>
2016-04-05 15:57:18 -0400

    add reference to current user on vote creation. Closes #10

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,7 +5,7 @@ class VotesController < ApplicationController
   end
 
   def create
-    Vote.create!(value: params[:value], link_id: params[:link_id])
+    Vote.create!(value: params[:value], link_id: params[:link_id], user_id: current_user.id)
     redirect_to :back
   end
 
